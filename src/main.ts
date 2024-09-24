935ed9caf86fc2ee79569723c735abfa6d230e68
ITSMEsouptik <108077353+ITSMEsouptik@users.noreply.github.com>
2024-09-24 17:34:18 +0530

    pushing to prod

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,24 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
 import * as dotenv from 'dotenv';
+import * as express from 'express'
+import serverlessExpress from '@vendia/serverless-express';
+import { ExpressAdapter } from '@nestjs/platform-express';
 
 dotenv.config();
+const expressApp = express();
 async function bootstrap() {
   const logger = new Logger();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  app.setGlobalPrefix('api');
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  const port = process.env.PORT
-  await app.listen(port);
-  logger.log(`Application listening to port ${3000}`)
+  await app.init();
+
+  const port = process.env.PORT || 3000;
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
+
+export const handler = serverlessExpress({app: expressApp})
